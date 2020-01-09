666aa98bbe87a04f685b30f044555dc78a80e980
WASWA OLUNGA <wolunga>
2020-01-08 19:52:14 -0800

    Add testing for keys

--- DataStructures/HashTable/TestImplementingAHashTable.ts
+++ DataStructures/HashTable/TestImplementingAHashTable.ts
@@ -5,8 +5,20 @@ const hashTable = new HashTable<number>(50);
 // * Error
 // hashTable.get(`grapes`);
 
-hashTable.set(`grapes`, 10000);
-console.log(hashTable.get(`grapes`));
+hashTable.set(`apples`, 10000);
+console.log(hashTable.get(`apples`));
+
+hashTable.set(`oranges`, 234);
+console.log(hashTable.get(`oranges`));
+
+hashTable.set(`plums`, 456);
+console.log(hashTable.get(`plums`));
+
+hashTable.set(`bananas`, 3445);
+console.log(hashTable.get(`bananas`));
+
+hashTable.set(`kiwis`, 1231);
+console.log(hashTable.get(`kiwis`));
 
 // * Same key different value should overwrite
 hashTable.set(`grapes`, 200);
@@ -15,3 +27,6 @@ console.log(hashTable.get(`grapes`));
 // * Addition results in collision
 hashTable.set(`grapesd`, 350);
 console.log(hashTable.get(`grapesd`));
+
+// * Get all keys
+console.log(hashTable.keys());
