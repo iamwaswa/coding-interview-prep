aaa1d848e3664ef971f25e81629dca5418b0a62b
Waswa <wolunga@sfu.ca>
2020-04-28 14:11:23 -0700

    Add QuickSort

---
+++ Algorithms/Sorting/QuickSort/ImplementQuickSort.ts
@@ -0,0 +1,28 @@
+// * Time Complexity: O(n^2)
+// * Space Complexity: O(log(n))
+export const quickSort = <T>(
+  items: Array<T>, 
+  left: number = 0, 
+  right: number = items.length - 1
+): Array<T> => {
+  if (right <= left) {
+    return items;
+  }
+
+  let pivotIndex = right;
+  for (let i = 0; i < pivotIndex; i++) {
+    if (items[i] > items[pivotIndex]) {
+      // * Swap items[i] with items[pivotIndex - 1]
+      [items[i], items[pivotIndex - 1]] = [items[pivotIndex - 1], items[i]];
+      // * Swap items[pivotIndex] with items[pivotIndex - 1]
+      [items[pivotIndex - 1], items[pivotIndex]] = [items[pivotIndex], items[pivotIndex - 1]];
+      pivotIndex--;
+      i--;
+    }
+  }
+
+  quickSort(items, left, pivotIndex - 1);
+  quickSort(items, pivotIndex + 1, right);
+  
+  return items;
+};
---
+++ Algorithms/Sorting/QuickSort/TestImplementingQuickSort.ts
@@ -0,0 +1,8 @@
+import { quickSort } from './ImplementQuickSort';
+
+let numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+console.log(quickSort(numbers));
+numbers = [];
+console.log(quickSort(numbers));
+numbers = [1];
+console.log(quickSort(numbers));
